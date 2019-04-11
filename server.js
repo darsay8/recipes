d410d8dfcfa550665453edfc84cb912ea48a43e4
darsay8 <darsay.one@gmail.com>
2019-04-11 07:46:59 -0400

    db connection

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "variables.env" });
+
+//Connects to database
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Database Connected :3"))
+    .catch(err => console.error(err));
+
+
 const app = express();
+
 
 const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
