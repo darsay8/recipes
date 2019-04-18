23eac6faf8c68a8788cc957f9bb4c0764f8fafc1
darsay8 <darsay.one@gmail.com>
2019-04-18 08:37:24 -0400

    signup form

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,10 +1,58 @@
 import React from "react";
 
 class Signup extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+  };
+
+  handleChange = e => {
+const{name, value} = e.target;
+
+this.setState({[name]: value})
+
+  }
+
   render() {
+    const {username, email, password, passwordConfirmation} = this.state;
     return (
-      <div>
-        <h1>Signup</h1>
+      <div className="App">
+        <h1 className="App">Signup</h1>
+        <form className="form">
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Confirm Password"
+            value={passwordConfirmation}
+            onChange={this.handleChange}
+          />
+          <button type="submit" className="button-primary">
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
